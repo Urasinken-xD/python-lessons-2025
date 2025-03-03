f3ed0fbcfb3c96aabffdd5e74c9ca84855eea3a5
Urasinken-xD <sobolev_m@1060.ru>
2025-03-03 15:13:22 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,15 +1,18 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 
 from config import TOKEN
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
+app = ApplicationBuilder().token("8145363054:AAHgz7xsLj5fTK6ywBYtfquOGBfxrFDSREs").build()
 
-app = ApplicationBuilder().token(TOKEN).build()
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'{update.message.text}')
 
 app.add_handler(CommandHandler("hello", hello))
+app.add_handler(MessageHandler(filters.ALL, echo))
 print('Бот запущен')
 app.run_polling()
 print('Бот приостоновлен')
