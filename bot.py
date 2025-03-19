64877ffd67ad5fef379811c6b6c5b21b4ddab51a
Urasinken-xD <sobolev_m@1060.ru>
2025-03-19 13:05:31 +0300

    добавил клавиатуру(не работает, пока что)

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import logging
 
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.constants import ParseMode
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters, CallbackQueryHandler
 
 from config import TOKEN
@@ -41,7 +42,26 @@ async def zuefa(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'{choice(zuefa_random)}')
 
 
-async def say_kyebord(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def say_help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Рассказывает, какие ф-ции доступны у бота"""
+    user = update.effective_user
+    log.info(f'Функция help вызвана пользователем {user}\n')
+
+    text = [
+        f'Привет, {user.first_name}! ',
+        'Я школьный бот, который может реагировать на следующие команды',
+        '/hello - говорю привет',
+        '/help, /start - покажу список доступных команд',
+        'Если хочешь поиграть со мной цуефа, просто напиши "камень", "ножницы" или "бумага"',
+        '/random_number - напишу случайное число',
+        '/say_keyboard - покажу клавиатуру'
+    ]
+    text = '\n'.join(text)
+
+    await update.message.reply_text(text)
+
+
+async def say_keybord(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Показывает клавиатуру"""
     user = update.effective_user
     log.info(f'Функция say_keybord вызвана пользователем {user}')
@@ -54,28 +74,35 @@ async def say_kyebord(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
          InlineKeyboardButton('Пять', callback_data='Пять')],
         [InlineKeyboardButton('Шесть', callback_data='Шесть')]
     ]
-    Keyboard = InlineKeyboardMarkup(buttons)
+    keyboard = InlineKeyboardMarkup(buttons)
+
+    await update.message.reply_text(
+        text='Выберите опцию на кклавиатуре',
+        reply_markup=keyboard
+    )
+
+
+async def react_keyboard(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Реагирует на нажатие кнопки на клавиатуре"""
+    user = update.effective_user
+    log.info(f'Функция react_keyboard вызвана пользователем {user}')
+
+    query = update.callback_query
+
+    buttons = [
+        [InlineKeyboardButton('Раз', callback_data='Раз'),
+         InlineKeyboardButton('Два', callback_data='Два'),
+         InlineKeyboardButton('Три', callback_data='Три')],
+        [InlineKeyboardButton('Четыре', callback_data='Четыре'),
+         InlineKeyboardButton('Пять', callback_data='Пять')],
+        [InlineKeyboardButton('Шесть', callback_data='Шесть')]
+    ]
+    keyboard = InlineKeyboardMarkup(buttons)
 
     await query.edit_message_text(
-                                )
-
-
-async def say_help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Рассказывает, какие ф-ции доступны у бота"""
-    user = update.effective_user
-    log.info(f'Функция help вызвана пользователем {user}\n')
-
-    text = [
-        f'Привет, {user.first_name}! ',
-        'Я школьный бот, который может реагировать на следующие команды',
-        '/hello - говорю привет',
-        '/help, /start - покажу список доступных команд',
-        'Если хочешь поиграть со мной цуефа, просто напиши "камень", "ножницы" или "бумага"',
-        '/random_number - напишу случайное число'
-    ]
-    text = '\n'.join(text)
-
-    await update.message.reply_text(text)
+        text=query.data,
+        reply_markup=keyboard
+    )
 
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -92,10 +119,10 @@ app = ApplicationBuilder().token(TOKEN).build()
 #Регестрация обработчиков
 app.add_handler(CommandHandler("Hello", hello))
 app.add_handler(CommandHandler(["Help", "Start"], say_help))
+app.add_handler(CommandHandler('keyboard', say_keybord))
 app.add_handler(CommandHandler("Random_number", random_number))
 app.add_handler(MessageHandler(filters.Text(["камень", "ножницы", "бумага"]), zuefa))
 app.add_handler(MessageHandler(filters.ALL, echo))
 print('Бот запущен')
 app.run_polling()
 print('Бот приостоновлен')
-
